a04c53b7bbe0369b8ab564f5057c5ca0a738f74e
hebertcisco <mrrabbitomg@gmail.com>
2021-05-11 22:52:01 -0300

    Refactoring to functional paradigm to close #2

--- install.sh
+++ install.sh
@@ -1,27 +1,61 @@
-#!/bin/bash
-## By Hebert F. Barros 2020
-## Removing any apt ## crashes
-sudo rm /var/lib/dpkg/lock-frontend
-sudo rm /var/cache/apt/archives/lock
+#!/bin/sh
+## By Hebert F. Barros 2021
 
-sudo apt-get update
+main() {
 
-# Install some required libraries
-sudo apt-get install php build-essential php-pear libaio1 alien
+    noAPT() {
+        echo 'Your sistem need the apitude to run this is script...'
+        exit
+    }
 
-# Download the oracle-instantclient-basic.
-echo 'Downloading the oracle-instantclient-basic.'
-wget https://download.oracle.com/otn_software/linux/instantclient/oracle-instantclient-basic-linuxx64.rpm
+    removingAptCrashes() {
+        echo 'Removing any apt crashes...'
+        sudo rm /var/lib/dpkg/lock-frontend
+        sudo rm /var/cache/apt/archives/lock
+        sudo apt-get update
+    }
 
-# Converting package .rpm to .deb.
-echo 'Converting package .rpm to .deb.'
+    installRequiredLibraries() {
+        # Install some required libraries
+        echo 'Installing some required libraries...'
+        sudo apt-get install php build-essential php-pear libaio1 alien
+    }
 
-sudo alien oracle-instantclient-basic-linuxx64.rpm && sleep 170
+    downloadTheOracleInstantClientBasic() {
+        # Download the oracle-instantclient-basic.
+        echo 'Downloading the oracle-instantclient-basic.'
+        wget https://download.oracle.com/otn_software/linux/instantclient/oracle-instantclient-basic-linuxx64.rpm
 
-# Installing oracle-instantclient-basic.deb.
-echo 'Installing oracle-instantclient-basic_21.1.0.0.0-2_amd64.deb.'
-sudo dpkg -i oracle-instantclient-basic_21.1.0.0.0-2_amd64.deb
+    }
+    convertingPackage() {
+        # Converting package .rpm to .deb.
+        echo 'Converting package .rpm to .deb.'
 
-# Removing packages.
-echo 'Removing packages.'
-rm -rf oracle-instantclient-basic-linuxx64.rpm oracle-instantclient-basic_21.1.0.0.0-2_amd64.deb
+        sudo alien oracle-instantclient-basic-linuxx64.rpm && sleep 170
+    }
+    installingTheOracleInstantClientBasic() {
+        # Installing oracle-instantclient-basic.deb.
+        echo 'Installing oracle-instantclient-basic_21.1.0.0.0-2_amd64.deb.'
+        sudo dpkg -i oracle-instantclient-basic_21.1.0.0.0-2_amd64.deb
+
+    }
+    removingPackages() {
+        # Removing packages.
+        echo 'Removing packages.'
+        rm -rf oracle-instantclient-basic-linuxx64.rpm oracle-instantclient-basic_21.1.0.0.0-2_amd64.deb
+
+    }
+    if ! [ -x "$(command -v apt)" ]; then
+        noAPT "$1" >&2
+        exit 1
+    else
+        removingAptCrashes "$1"
+        installRequiredLibraries "$1"
+        downloadTheOracleInstantClientBasic "$1"
+        convertingPackage "$1"
+        installingTheOracleInstantClientBasic "$1"
+        removingPackages "$1"
+    fi
+}
+
+main "$1"
